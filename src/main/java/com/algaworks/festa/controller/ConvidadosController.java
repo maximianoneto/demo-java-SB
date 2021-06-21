56f15de2a850929507c1692c6463c785044594a2
Maximiano Cid Netoo <nic.neto@hotmail.com>
2021-06-21 12:18:47 -0300

    First Commit

---
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -0,0 +1,37 @@
+package com.algaworks.festa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import com.algaworks.festa.model.Convidado;
+import com.algaworks.festa.repository.Convidados;
+
+@Controller
+@EnableAutoConfiguration
+public class ConvidadosController {								
+	@Autowired
+	private Convidados convidados;
+	
+	@GetMapping("/convidados")
+	public ModelAndView listar() {
+		
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		modelAndView.addObject("convidados", convidados.findAll());
+		modelAndView.addObject(new Convidado());
+		return modelAndView;
+		
+		}
+	
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
+	}
+
+}
+	
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO convidado (id, nome, quantidade_acompanhantes) VALUES (1, 'Pedro', 2);
+
+INSERT INTO convidado (id, nome, quantidade_acompanhantes) VALUES (2, 'Maria', 3);
+
+INSERT INTO convidado (id, nome, quantidade_acompanhantes) VALUES (3, 'Ricardo', 1);
